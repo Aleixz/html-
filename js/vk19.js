f525800c29200fc800dff23f3641fa236e3308a5
Aleixz <104515147+Aleixz@users.noreply.github.com>
2023-06-08 08:37:38 +0800

    Update vk19.js

--- js/vk19.js
+++ js/vk19.js
@@ -24,6 +24,7 @@ async function sendMessage() {
       inputContainer.style.display = "flex";
       button.style.display = "block";
       typingContainer.style.display = "none";
+      input.focus(); // 自动选中输入框
     }, 1000);
   }
 }
